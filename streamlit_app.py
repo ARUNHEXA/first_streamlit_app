e6a5825c3b5e0eb3940767a495a1a43bd9fa66f6
ARUNHEXA <144514719+ARUNHEXA@users.noreply.github.com>
2023-09-09 15:10:21 -0500

    Update streamlit_app.py
    Added lines for text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast')
+streamlit.text('Oats breakfast')
+streamlit.text('Poha')
+streamlit.text('Khichdi')
