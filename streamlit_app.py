f328115fb5166dc7b24484a347bc58c5c013e6e6
ARUNHEXA <144514719+ARUNHEXA@users.noreply.github.com>
2023-09-15 18:06:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,10 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json()) #just write data to screen
 
+#add a text entry box and send the input to fruityvice as part of the API call
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered',fruit_choice)
+
 #take the jason version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output on the screen as a table
