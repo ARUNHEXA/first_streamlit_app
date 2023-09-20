d99f830d8447e89b8b2da2899712806414784446
ARUNHEXA <144514719+ARUNHEXA@users.noreply.github.com>
2023-09-20 00:05:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,5 @@ streamlit.dataframe(my_data_row)
 #adding additional text box
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding',add_my_fruit)
+
+my_cur.execute("sinsert into fruit_load_list values('from_streamlit')")
