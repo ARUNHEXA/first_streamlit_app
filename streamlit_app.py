f1e947e6cc1a2a9d938635c6ff8a273b9df0b97b
ARUNHEXA <144514719+ARUNHEXA@users.noreply.github.com>
2023-09-20 00:16:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My parents new healthy diner')
 
@@ -10,8 +14,8 @@ streamlit.text(' 🥑🍞 Guacamole')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
 
+#imprt pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -23,7 +27,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-import requests
+#import requests
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json()) #just write data to screen
@@ -37,8 +41,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output on the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
 
+#import snowflake connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
